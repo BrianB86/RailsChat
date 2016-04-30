7d608446d063902273fd45c6d47a2d50cf218f79
BrianB86 <bbattell86@gmail.com>
2016-04-30 12:21:48 -0400

    fixed spacing on chat area

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,7 +1,15 @@
 class ChatController < ApplicationController
 
-  def railsChat #This is our area for the chat client.
+  def railsChat #This is our area for the chat client. initialize subscriber here.
     redirect_to root_url unless logged_in?
     @chatUsers = User.select(:name).where("user_action_time_stamp > ? AND login_notice = ?", DateTime.now - 15.minutes, true ) #maybe add find_each
+  end
+
+  def show #show all messages
+
+  end
+
+  def create #create a message and #notify publish users that a new message was posted.
+
   end
 end
