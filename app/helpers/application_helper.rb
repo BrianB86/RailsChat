baf827b69a81b6d7c27707165416b5578fd33db5
BrianB86 <bbattell86@gmail.com>
2016-05-01 14:27:32 -0400

    added defense against unauthorized faye requests.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def broadcast(channel, &block)
-    message = {:channel => channel, :data => capture(&block)}
+    message = {:channel => channel, :data => capture(&block), :ext => {:auth_token => FAYE_TOKEN}}
     uri = URI.parse("http://localhost:9292/faye")
     Net::HTTP.post_form(uri, :message => message.to_json)
   end
